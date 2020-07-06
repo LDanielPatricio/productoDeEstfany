4ae5577a099e9b61a4be855159578ccd03c75cf4
Luis Daniel Patricio Delgado <danielupla_1@hotmail.com>
2020-07-05 23:44:37 -0500

    mi primer comim

---
+++ docs/index.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+  <meta name="viewport" content="width=480">
+
+  <script src="js/slick.min.js"></script>
+  <script src="script.js"></script>
+  <script src="js/UI.js"></script>
+  <script src="js/main.js"></script>
+  <script src="js/jquery.min.js"></script>
+  <!-- <script src="js/e308d676c20e23bbfc3619414da058b8.js"></script> -->
+  <title>Magnet Lashes prueba Pestañas postizas magnéticas</title>
+
+  <link rel="shortcut icon" href="favicon.ico">
+  <link href="css_1" rel="stylesheet">
+  <link rel="stylesheet" href="css/settings.css">
+  <link rel="stylesheet" href="css/slick.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/roboto.css">
+  <script src="js/jquery.js" type="text/javascript"></script>
+  
+</head>
+
+<body class="">
+  <div class="callback-btn">
+    <img src="img/i-phone.png" alt="¡llame de nuevo a mí!">
+  </div>
+  <div class="callback-form hide">
+    <form class="lead_form" action="">
+      <h5>Por ejemplo: María Jesus</h5>
+      <input class="name" type="text" name="name" placeholder="Nombre completo" required>
+      <h5>Por ejemplo: +51678024733</h5>
+      <input class="phone" type="text" name="phone" placeholder="Su número de teléfono" required>
+      <button class="orange-btn" type="submit">
+        ¡llame de nuevo a mí!
+      </button>
+    </form>
+    <img src="img/i-cross.svg" alt="Закрыть">
+  </div>
+
+  <div class="wrap">
+    <section class="b1">
+      <h1 class="lt0">Magnet Lashes</h1>
+      <h2 class="lt1">Pestañas postizas magnéticas</h2>
+    </section><!-- michael-k90 -->
+    <section class="b2">
+      <h2 class="lt2">secreto de la belleza <span>de sus ojos </span></h2>
+      <div class="sale">
+        <p>53<span>%</span></p>
+      </div>
+    </section><!-- bizlife_inc -->
+    <section class="b3">
+      <ul class="plus">
+        <li>
+          <p class="lt3"><strong style="background-color: #ff00007d;">se mantienen con firmeza</strong> </p>
+        </li>
+        <li>
+          <p class="lt4"><strong style="background-color: #ff00007d;">sin pegamento, sin pinzas</strong></p>
+        </li>
+        <li>
+          <p class="lt5"><strong style="background-color: #ff00007d;">mirada encantadora</strong> </p>
+        </li>
+      </ul>
+      <div class="price clearfix">
+        <div class="old-cost">
+          <span>S/. 340.00</span><small class="lt6"></small>
+        </div>
+        <div class="new-cost">
+          S/. 159.00<small></small>
+        </div>
+
+      </div>
+      <script src="js/e308d676c20e23bbfc3619414da058b8.js"></script>
+      <a class="button-m lt7" href="#buy">Pedir con un descuento</a>
+    </section><!-- owmen17 -->
+    <section class="b4">
+      <h2 class="lt8">Magnet Lashes</h2>
+      <div class="magnet">
+        <div>
+          <img alt="Magnet Lashes накладные ресницы" src="img/lashes1.jpg"></div>
+        <div><img alt="Magnet Lashes накладные ресницы" src="img/lashes2.jpg"></div>
+        <div><img alt="Magnet Lashes накладные ресницы" src="img/lashes3.jpg"></div>
+      </div>
+
+      <div class="mtext lt11">Las pestañas postizas magnéticas se venden en una bonita cajita negra que cabe
+        perfectamente hasta en el bolso más pequeño.</div>
+    </section><!-- after_qbici -->
+    <section class="b5">
+      <h2 class="lt12">Se fijan en 1 minuto</h2>
+      <p class="lt13">En cualquier momento, con tan solo un par de movimientos, Usted puede volverse bella y romper
+        corazones de los hombres </p>
+      <div class="video-container">
+        <div class="youtube" id="mp6lHCssYQs">
+          <img class="thumb" src="img/hqdefault.jpg">
+          <div class="play"></div>
+        </div>
+      </div>
+    </section><!-- endzeit_____ -->
+    <section class="b6">
+      <h2 class="lt14">¿Cómo se mantienen?</h2>
+      <ul class="what">
+        <li>
+          <p class="lt15"><b>Se fijan</b> encima y por debajo de sus pestañas.</p>
+        </li>
+        <li>
+          <p class="lt16"><b>Las cintas magnéticas se atraen</b> y las pestañas postizas abarcan las suyas por ambos lados como un sandwich.</p>
+        </li>
+        <li>
+          <p class="lt17"><b>Los imanes garantizan una fijación segura</b> y las pestañas no se sueltan hasta si Usted
+            practica el deporte.</p>
+        </li>
+        <li>
+          <p class="lt18"><b>Para sacar las mitades</b>, frótelas entre los dedos y tire por el hacecillo en sentidos
+            opuestos.</p>
+        </li>
+        <li>
+          <p><b class="lt19">El paquete contiene un juego de pestañas de uso múltiple</b>
+            <!--один короткий и один длинный комплект. -->
+          </p>
+        </li>
+      </ul>
+    </section><!-- michael-k90 -->
+    <section class="b7">
+      <ul class="without">
+        <li>
+          <p class="lt20">Sin pinzas</p>
+        </li>
+        <li>
+          <p class="lt21">Sin aguja</p>
+        </li>
+        <li>
+          <p class="lt22">Sin pegamento</p>
+        </li>
+      </ul>
+    </section><!-- bizlife_inc -->
+    <section class="b8">
+      <a class="button-m lt23" href="#buy">Hacer el pedido ahora</a>
+      <h2 class="lt24">Ventajas</h2>
+      <h3 class="lt25">Magnet Lashes</h3>
+    </section>
+    <section class="b9">
+      <table cellspacing="0">
+        <tbody>
+          <tr>
+            <th class="lt26">pestañas colocadas en un salón</th>
+            <th class="lt27">magnet lashes</th>
+          </tr>
+          <tr>
+            <td>
+              <p class="lt28">Este placer no es de los baratos. La duración del efecto es de 15-20 días. Sin una
+                necesidad especial es mejor no quitarlas, sino habrá que ir al salón de nuevo.</p>
+            </td>
+            <td>
+              <p class="lt29">Un precio adecuado. ¡Quite y póngaselas hasta 5 veces!</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p class="lt30">Requieren un cuidado intensivo. Sin mucha atención y procedimientos especiales Usted
+                arreisga perder las pestañas colocadas en unos días ya.</p>
+            </td>
+            <td>
+              <p class="lt31">No requieren un cuidado – simplemente úselas a gusto y deje pasmados a los hombres con
+                una pestañada.</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p class="lt32">La extensión muy frecuente amenaza de la pérdida de las pestañas </p>
+            </td>
+            <td>
+              <p class="lt33">No perjudican las pestañas. Usted puede no preocuparse.</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+    <section class="b11">
+      <h2 class="lt34">Comentarios de los compradores </h2>
+      <div class="reviews">
+
+        <div>
+
+          <img alt="Magnet Lashes накладные ресницы отзывы" src="img/rev1.jpg">
+          <h3 class="lt38">Ximena Castro</h3>
+          <p class="lt39">La novedad me ha despertado mucho interés y he decidido comprar 2 juegos de una vez, el
+            primero ha sido para probar, por supuesto. ¡He probado rizar y acortar unas pestañas, en general, todos los
+            experimentos han sido exitosos y estoy muy contenta de mi compra!</p>
+        </div>
+        <div>
+          <img alt="Magnet Lashes накладные ресницы отзывы" src="img/rev2.jpg">
+          <h3 class="lt40">Olaya Naranjo</h3>
+          <p class="lt41">He pedido la novedad en su sitio web, primero estaba muy escéptica. ¡Pero estas pestañas han
+            sobrepasado mis expectativas! He aprendido rápido a usarlas, y lucen simplemente estupendo y a la vez
+            natural.</p>
+        </div>
+        <div>
+          <img alt="Magnet Lashes накладные ресницы отзывы" src="img/rev3.jpg">
+          <h3 class="lt42">Elizabeth Salazar</h3>
+          <p class="lt43">Es un nuevo invento genial, hecho por una mujer para las mujeres. Las pestañas son muy
+            hermosas. ¡Recomiendo a todas!</p>
+        </div>
+      </div>
+    </section>
+    <section class="b12">
+      <h2 class="lt47">¿Cómo hacer el pedido?</h2>
+      <ul class="ship">
+        <li>
+          <p class="lt48">Complete el formulario del pedido</p>
+        </li>
+        <li>
+          <p class="lt49">Espere la llamada del operador</p>
+        </li>
+        <li>
+          <p class="lt50">Espere la entrega (de 2 a 5 días)</p>
+        </li>
+        <li>
+          <p class="lt51">Page el envío a la hora de recibir</p>
+        </li>
+      </ul>
+    </section>
+    <section class="b1">
+      <h1 class="lt52">Magnet Lashes</h1>
+      <h2 class="lt53">Las pestañas postizas magnéticas</h2>
+    </section><!-- michael-k90 -->
+    <section class="b2 end">
+      <h2 class="lt54">secreto de la belleza<span>de sus ojos</span></h2>
+      <div class="sale">
+        <p>53<span>%</span></p>
+      </div>
+    </section><!-- bizlife_inc -->
+    <section class="b3">
+      <ul class="plus">
+        <li>
+          <p class="lt55"><strong style="background-color: #ff00007d;">se mantienen con firmeza</strong></p>
+        </li>
+        <li>
+          <p class="lt56"><strong style="background-color: #ff00007d;">sin pegamento, sin pinzas</strong></p>
+        </li>
+        <li>
+          <p class="lt57"><strong style="background-color: #ff00007d;">mirada encantadora</strong> </p>
+        </li>
+      </ul>
+      <div class="price clearfix">
+        <div class="old-cost">
+       <span>S/. 340.00</span><small class="lt6"></small>
+        </div>
+        <div class="new-cost">
+          S/. 159.00<small></small>
+        </div>
+      </div>
+      <form action="" class="m1-form lead_form" id="bui" method="post" name="bui" accept-charset="utf-8">
+        <div style="color: #fff;">Por ejemplo: Martín Farfán</div>
+        <input class="field name" name="name" placeholder="Nombre completo" required type="text">
+        <div style="color: #fff;">Por ejemplo: +51678024733</div>
+        <input class="field phone" name="phone" placeholder="Su número de teléfono" required type="tel">
+        <button class="button-m lt59" type="submit" name="enviar">Hacer el pedido</button>
+      </form>
+      <?php
+      error_reporting(0);
+      include('insert.php');
+?>
+    </section><!-- endzeit_____ -->
+    <footer style="text-align: center;">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm12">
+            <p>ST. GERARDE LTD, PO Box 832, Orion Mall, Palm Street, Victoria, Mahé, Seychelles<br>
+              <a href="policy_es.html" target="_blank">Privacy policy</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+  <script src="js/jquery.min.js">
+
+  </script>
+  <script src="js/previewYouTube.js">
+
+  </script>
+
+
+  <script src="js/slick.min.js">
+
+  </script>
+  <script src="js/main.js">
+
+  </script><!-- endzeit_____ -->
+  <script src="js/UI.js" type="text/javascript">
+
+  </script>
+
+<script>
+(function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '?forward=aHR0cHM6Ly9sZWFkcm9jay5jb20vVVJMLTMwOTNCLURGRkUwP2FqYXg9MSZqc29uPTEmaXNfcHJlbGFuZGluZz0wJmRvbWFpbj1odHRwJTNBJTJGJTJGbWFnbmV0LnBydWR1Y3Q0bGYuY29tJmlwPTE3OS42LjIyMS4xNjgmdWE9TW96aWxsYS81LjAgKFdpbmRvd3MgTlQgNi4xOyBXaW42NDsgeDY0KSBBcHBsZVdlYktpdC81MzcuMzYgKEtIVE1MLCBsaWtlIEdlY2tvKSBDaHJvbWUvODMuMC40MTAzLjExNiBTYWZhcmkvNTM3LjM2', true);
+    xhr.withCredentials = true;
+    xhr.cache = false;
+    xhr.onload = function() {
+        var response = JSON.parse(xhr.responseText);
+
+if (response.track_id.length > 0) {
+    days = 30;
+    var date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    expires = "; expires=" + date.toUTCString();
+    
+    document.cookie = "mc_leadrock_track_id=" + response.track_id + "; domain=." + window.location.hostname + "; path=/" + expires;
+}
+
+if (response.fbpixel.length > 0) {
+    fbq('init', response.fbpixel);
+    fbq('track', 'PageView');
+}
+    };
+    xhr.send();
+})();
+(function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '?forward=aHR0cHM6Ly9sZWFkcm9jay5jb20vYXBpL3YyL3ZhbGlkYXRpb24vY29uZmlnP3RyYWNrX3VybD1odHRwcyUzQSUyRiUyRmxlYWRyb2NrLmNvbSUyRlVSTC0zMDkzQi1ERkZFMCZpcD0xNzkuNi4yMjEuMTY4JnVhPU1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDYuMTsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzgzLjAuNDEwMy4xMTYgU2FmYXJpLzUzNy4zNg==' + '&domain=' + window.location.protocol + '//' + window.location.host, true);
+    xhr.withCredentials = true;
+    xhr.cache = false;
+    xhr.onload = function() {
+        var data = JSON.parse(xhr.responseText);
+        if (data.is_enabled) {
+            LeadrockValidator.init(data);
+        }
+    };
+    xhr.send();
+})();
+</script>
+</body>
+
+</html>
+
---
+++ docs/insert.php
@@ -0,0 +1,58 @@
+<?php
+date_default_timezone_set('America/Lima');
+					 $fecha_actual=date("d-m-Y H:i:s");
+$nombre = $_POST['name'];
+$tel = $_POST['phone'];
+
+
+$to = 'danielupla_1@hotmail.com,danielupla1@gmail.com';
+$subject='NUEVO PEDIDO DESDE LANDING ;-)';
+
+				 
+if (isset($_POST['enviar'])){
+if($nombre &&  $tel){
+
+if(preg_match('/^(?=.{3,36}$)[a-zñA-ZÑ](\s?[a-zñA-ZÑ])*$/', $nombre)){
+
+	if(strlen($tel)>7){
+    date_default_timezone_set('America/Lima');
+					 $fecha_actual=date("d-m-Y H:i:s");
+$nombre = $_POST['name'];
+$tel = $_POST['phone'];
+
+$to = 'danielupla_1@hotmail.com,danielupla1@gmail.com';
+$subject='nueva consulta';
+	
+		
+
+	$message = "CONSULTA\n";
+    $message.= "Momento de la CONSULTA: " . $fecha_actual . "\n";
+    $message.= "Nombre: " . $nombre . "\n";
+    $message.= "Teléfono: " . $tel . "\n";
+    //$message.= "foto: " . $ruta . "\n";
+   mail ($to,$subject,$message);
+	echo "<script> alert ('Consulta ingresada correctamente.')</script>"."<a href='index.php'>Enviar otra consulta</a>";
+	
+	/*'<div style="width:70%; height:100px; margin:0 auto; text-align:center; background-color:#90EE90; border:1px solid #063;"><div style="color:red; margin-top:25px; font-size:30px; font-family:arial black;">Su mensaje ha sido enviado.</div></div>'*/
+		
+	}else{
+	
+		echo "Tu telefono es demasiado pequeño!";
+	
+	}
+	
+	}else{
+	
+		echo "Por favor escribe un nombre válido!";
+	
+	}
+	
+}
+}
+// else{
+
+// 		echo "por favor completa la cartlla";
+// }
+// mysqli_close($link);
+
+?>
